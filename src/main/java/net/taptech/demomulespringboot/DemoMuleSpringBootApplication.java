418308588a38756cba761ede53f808877a8f8db4
Greg Lawson <Gregory.Lawson@altisource.com>
2018-01-21 17:00:30 -0500

    Using 1.0.0 mule-spring-boot-starter

---
+++ src/main/java/net/taptech/demomulespringboot/DemoMuleSpringBootApplication.java
@@ -0,0 +1,29 @@
+package net.taptech.demomulespringboot;
+
+import net.taptech.autoconfiguration.EnableMuleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@EnableMuleConfiguration
+@SpringBootApplication
+public class DemoMuleSpringBootApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoMuleSpringBootApplication.class);
+
+	@Autowired
+	private ApplicationContext context;
+
+	public static void main(String... args) {
+		logger.info("Starting SpringApplication...");
+		SpringApplication app = new SpringApplication(DemoMuleSpringBootApplication.class);
+		app.setBannerMode(Banner.Mode.CONSOLE);
+		app.setWebEnvironment(false);
+		app.run();
+		logger.info("SpringApplication has started...");
+	}
+}
